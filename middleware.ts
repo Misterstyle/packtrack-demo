efc98e869ca52ec37a1eb3736cafba8a1a573114
Misterstyle <emrahkoc@live.nl>
2026-02-12 23:08:48 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export function middleware() {
+export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
 
