4a6b6a1a92e55fad88f7f1f2983fcdf3781d76d3
Misterstyle <emrahkoc@live.nl>
2026-02-12 23:03:39 +0100

    Update middleware.ts
    nodige update

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,5 @@
+import { NextResponse } from 'next/server'
 
-
-import { type NextRequest } from "next/server"
-import { updateSession } from "@/lib/supabase/middleware"
-
-export const runtime = 'nodejs';
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
-}
-
-export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+export function middleware() {
+  return NextResponse.next()
 }
